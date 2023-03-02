8aab1126719b5db094529a053862f40b9a151e34
Jojow-World <jonatas-fab@hotmail.com>
2023-03-02 10:29:12 +0000

    att game

--- pyton.alura.py
+++ pyton.alura.py
@@ -1,16 +1,27 @@
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
+
 numero_secreto = 42
 total_de_tentativas = 3
 rodada = 1
 
-while (rodada <= total_de_tentativas):
-    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+#valores = ['gabirle', 'jonata']
 
-    chute_str = input("Digite o seu número: ")
+#for nome in valores:
+ #   print(nome)
+
+for rodada in range(1,total_de_tentativas + 1):
+    #print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    print(f"Tentativa {rodada} de {total_de_tentativas}")
+
+    chute_str = input("Digite um numero de 1 a 100: ")
     print("Você digitou " , chute_str)
     chute = int(chute_str)
+    
+    if(chute < 1 or chute > 100):
+        print("Digite apenas números de 1 a 100!!!")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -18,12 +29,12 @@ while (rodada <= total_de_tentativas):
 
     if(acertou):
         print("Parabéns! Você acertou!")
+        break
     else:
         if(maior):
             print("O seu chute foi maior do que o número secreto!")
         elif(menor):
             print("O seu chute foi menor do que o número secreto!")
 
-    rodada = rodada + 1
 
 print("Fim do jogo")
